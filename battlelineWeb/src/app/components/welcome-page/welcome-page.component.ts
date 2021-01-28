ddbb165bafdc0a394d12daba1b7bf5981f12f415
Hirunz <54896502+Hirunz@users.noreply.github.com>
2021-01-28 17:25:19 +0530

    added welcome function
    added a function to display Mp page component on timeout.

--- battlelineWeb/src/app/components/welcome-page/welcome-page.component.ts
+++ battlelineWeb/src/app/components/welcome-page/welcome-page.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
-
+  
+  public contentVisibiliy=false;
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(()=> this.contentVisibiliy=true,10000);
   }
 
 }
