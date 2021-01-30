73354757ff3913bf40a215549966a1d40ac6f3cf
Ramindu2020 <ramindu.2019730@iit.ac.lk>
2021-01-30 17:07:50 +0530

    header component updated

--- battlelineWeb/src/app/components/header/header.component.ts
+++ battlelineWeb/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() gameMode!: string;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() {
+  }
 }
