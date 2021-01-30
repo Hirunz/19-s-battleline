29d138c4975239722a7d1572a909af65181b8f09
Ramindu2020 <ramindu.2019730@iit.ac.lk>
2021-01-30 15:37:04 +0530

    header component added

--- battlelineWeb/src/app/app.module.ts
+++ battlelineWeb/src/app/app.module.ts
@@ -42,6 +42,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
@@ -52,6 +53,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     NavBarComponent,
     FooterComponent,
     MpScoresComponent,
+    HeaderComponent,
     
   
   ],
