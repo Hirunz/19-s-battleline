b5601c62ffc03809545e377292086a5575f4c587
Hirunz <54896502+Hirunz@users.noreply.github.com>
2021-01-30 19:16:15 +0530

    temp commit

--- battlelineWeb/src/app/components/admin-view/mp/mp-scores/mp-scores.component.ts
+++ battlelineWeb/src/app/components/admin-view/mp/mp-scores/mp-scores.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IMpMatch } from 'src/app/Interfaces/IMpMatch';
+import { IMPTeam } from 'src/app/Interfaces/IMPTeam';
+import data from '../../../../../assets/files/data.json'
 
 @Component({
   selector: 'app-mp-scores',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MpScoresComponent implements OnInit {
 
+  public matches:IMpMatch[] = []
+  public teams: IMPTeam[] = []
+
   constructor() { }
 
   ngOnInit(): void {
+     
   }
+
+  readFromFile(){
+  
+  }
+  saveInFile(){
+
+  }
+
+
 
 }
