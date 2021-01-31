cd7842e134d468c5790db3cb7abf603f7ea0b3e1
Ramindu2020 <ramindu.2019730@iit.ac.lk>
2021-01-31 20:18:39 +0530

    br-scores updated

--- battlelineWeb/src/app/components/admin-view/br/br-scores/br-scores.component.ts
+++ battlelineWeb/src/app/components/admin-view/br/br-scores/br-scores.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrScoresComponent implements OnInit {
 
-  IBrPlayer
+  public playerList:IBrPlayer[] = [];
   
   constructor() { }
 
